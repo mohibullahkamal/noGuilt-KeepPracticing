66944964cd0514c543ce2ed24d3fabc7cc70f050
mogit2020 <mohibullah.kamal@gmail.com>
2020-11-08 12:38:41 +0100

    added methods for get, post, update, and delete.

--- Express-Project/index.js
+++ Express-Project/index.js
@@ -4,11 +4,34 @@ const app = express()
 const port = 3000
 
 
-
-
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: path.join(__dirname, './')})
 })
+
+
+
+//Respond with Hello World! on the homepage:
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+//Respond to POST request on the root route (/), the application’s home page:
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+//Respond to a PUT request to the /user route:
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+//Respond to a DELETE request to the /user route:
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
